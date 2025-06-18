f0a1aa4b865110e80324c42239f4ee5c0ea09630
BushinyanForSchool <0021k7810235y2q4@gl.pen-kanagawa.ed.jp>
2025-06-18 14:11:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const ctx = cvs.getContext('2d');
 const canvasUpdate = () => {
    cvs.width = contentWidth;
    cvs.height = contentHeight;
-   ctx.drawImage(video, 0, 0, window.innerWidth, window.innerHeight);
+   ctx.drawImage(video, 0, 0, contentWidth*2, contentHeight*2);
    requestAnimationFrame(canvasUpdate);
 }
 
